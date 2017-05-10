45bf4821a2863d7b296ec46fb115a789499ea3f2
neeraj399 <neeraj.399@gmail.com>
2017-05-09 23:09:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+var http = require('http');
 
+var server = http.createServer(function(request, response) {
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write("nee");
+		response.end();
+});
+
+server.listen(5000, function() {
+});
